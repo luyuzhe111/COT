df7d6ff2ef57cdd19c19c8045134733f4f879710
luyuzhe111 <yuzhe.lu@vanderbilt.edu>
2023-01-08 15:55:58 -0800

    local change

--- experiment_baselines.sh
+++ experiment_baselines.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #SBATCH -N 1
-#SBATCH -t 10:00:00
+#SBATCH -t 4:00:00
 #SBATCH --export=ALL
 #SBATCH --exclusive
 
 source ~/.bashrc
 conda activate ood
 
-cifar_data_path="./data/CIFAR-100"
-cifar_corruption_path="./data/CIFAR-100-C"
-n_class=100
+cifar_data_path="./data/CIFAR-10"
+cifar_corruption_path="./data/CIFAR-10-C"
+n_class=10
 seed=1
 model_seed="1_15"
 
-for arch in resnet18
+for arch in resnet18 resnet50 vgg11
 
 do
     python run_baselines.py --corruption clean --severity 0 --num_classes ${n_class} --arch ${arch} --model_seed ${model_seed} --seed ${seed} --cifar_data_path ${cifar_data_path} --cifar_corruption_path ${cifar_corruption_path}
