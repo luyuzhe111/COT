2d6e864cdf262af33cae1c09108e41b1329d2e81
luyuzhe111 <yuzhe.lu@vanderbilt.edu>
2023-01-29 15:39:05 -0800

    refactor & sample complexity exp

--- load_data.py
+++ load_data.py
@@ -1,110 +1,15 @@
 import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import CIFAR10, CIFAR100
+from torch.utils.data import Dataset
 import os
 import numpy as np
 from torch_datasets.tiny_imagenet import *
-from torch_datasets.configs import get_train_val_size
 from torch_datasets.cifar20 import CIFAR20, get_coarse_labels
 
 
-# def load_image_dataset(corruption_type,
-#                        clean_path,
-#                        corruption_path,
-#                        corruption_severity=0,
-#                        split='test',
-#                        num_samples=50000,
-#                        dsname="cifar-10",
-#                        seed=1):
-
-
-def load_train_dataset(split, configs):
-
-    assert split in ['train', 'test'], 'unknown split'
-    training_flag = True if split == 'train' else False
-
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    transform = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std),
-    ])
-
-    dsname = configs.dataset
-
-    if dsname == "cifar-10":
-        dataset = CIFAR10(configs.clean_path, train=training_flag, transform=transform, download=True)
-    
-    elif dsname == "cifar-20":
-        dataset = CIFAR20(configs.clean_path, train=training_flag, transform=transform, download=True)
-
-    elif dsname == "cifar-100":
-        dataset = CIFAR100(configs.clean_path, train=training_flag, transform=transform, download=True)
-    
-    elif dsname == 'tiny-imagenet':
-        dataset = TinyImageNet(configs.clean_path, split=split, transform=transform)
-    
-    else:
-        raise ValueError('unknown dataset')
-
-    
-    if corruption_severity > 0:
-        assert not training_flag
-        if dsname in ['cifar-10', 'cifar-100']:
-            path_images = os.path.join(corruption_path, corruption_type + '.npy')
-            path_labels = os.path.join(corruption_path, 'labels.npy')
-            dataset.data = np.load(path_images)[(corruption_severity - 1) * 10000:corruption_severity * 10000]
-            dataset.targets = list(np.load(path_labels)[(corruption_severity - 1) * 10000:corruption_severity * 10000])
-            dataset.targets = [int(item) for item in dataset.targets]
-        
-        elif dsname == 'cifar-20':
-            path_images = os.path.join(corruption_path, corruption_type + '.npy')
-            path_labels = os.path.join(corruption_path, 'labels.npy')
-            dataset.data = np.load(path_images)[(corruption_severity - 1) * 10000:corruption_severity * 10000]
-            dataset.targets = list(get_coarse_labels(np.load(path_labels))[(corruption_severity - 1) * 10000:corruption_severity * 10000])
-            dataset.targets = [int(item) for item in dataset.targets]
-
-        elif dsname == 'tiny-imagenet':
-            dataset = TinyImageNetCorrupted(corruption_path, corruption_type, corruption_severity, transform=transform)
-
-
-    # randomly permute data
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    number_samples = len(dataset.data)
-    index_permute = torch.randperm(number_samples)
-    dataset.data = [dataset.data[i] for i in index_permute]
-    # dataset.targets = np.array([int(item) for item in dataset.targets])
-    dataset.targets = [dataset.targets[i] for i in index_permute]
-
-    train_size, val_size = get_train_val_size(dsname)
-    if training_flag:
-        train_inds = index_permute[:train_size]
-        val_inds = index_permute[train_size:]
-
-        train_set = torch.utils.data.Subset(dataset, train_inds)
-        val_set = torch.utils.data.Subset(dataset, val_inds)
-
-        print("train size:", len(train_set))
-        print("valid size:", len(val_set))
-
-        return train_set, val_set
-
-    else:
-        # randomly subsample data
-        torch.manual_seed(seed)
-        torch.cuda.manual_seed(seed)
-        if num_samples < 10000:
-            indices = torch.randperm(10000)[:num_samples]
-            dataset = torch.utils.data.Subset(dataset, indices)
-            print('number of test data: ', len(dataset))
-
-        return dataset
-
-
-def load_train_dataset(configs):
-    if configs.use_pretrained_weights:
+def load_train_dataset(dsname, iid_path, n_val_samples, seed=1, pretrained=True):
+    if pretrained:
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
         transform = transforms.Compose([
@@ -118,25 +23,22 @@ def load_train_dataset(configs):
             transforms.ToTensor(),
         ])
 
-    dsname = configs.dataset
-
-    if dsname == "cifar-10":
-        dataset = CIFAR10(configs.clean_path, train=True, transform=transform, download=True)
+    if dsname == "CIFAR-10":
+        dataset = CIFAR10(iid_path, train=True, transform=transform, download=True)
     
-    elif dsname == "cifar-20":
-        dataset = CIFAR20(configs.clean_path, train=True, transform=transform, download=True)
+    elif dsname == "CIFAR-20":
+        dataset = CIFAR20(iid_path, train=True, transform=transform, download=True)
 
-    elif dsname == "cifar-100":
-        dataset = CIFAR100(configs.clean_path, train=True, transform=transform, download=True)
+    elif dsname == "CIFAR-100":
+        dataset = CIFAR100(iid_path, train=True, transform=transform, download=True)
     
-    elif dsname == 'tiny-imagenet':
-        dataset = TinyImageNet(configs.clean_path, split='train', transform=transform)
+    elif dsname == 'Tiny-ImageNet':
+        dataset = TinyImageNet(iid_path, split='train', transform=transform)
     
     else:
         raise ValueError('unknown dataset')
 
-    assert configs.n_val_samples > 0, 'no validation set'
-    seed = configs.split_seed
+    assert n_val_samples > 0, 'no validation set'
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     n_samples = len(dataset.data)
@@ -144,7 +46,7 @@ def load_train_dataset(configs):
     dataset.data = [dataset.data[i] for i in index_permute]
     dataset.targets = [dataset.targets[i] for i in index_permute]
 
-    train_size = n_samples - configs.n_val_samples
+    train_size = n_samples - n_val_samples
 
     train_inds = index_permute[:train_size]
     val_inds = index_permute[train_size:]
@@ -156,3 +58,62 @@ def load_train_dataset(configs):
     print("valid size:", len(val_set))
 
     return train_set, val_set
+
+
+def load_test_dataset(dsname, iid_path, corr_path, corr_type, corr_sev, n_test_sample, seed=1, pretrained=True):
+    if pretrained:
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean, std),
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.ToTensor(),
+        ])
+
+    # test on in-distribution data
+    if dsname == "CIFAR-10":
+        dataset = CIFAR10(iid_path, train=False, transform=transform, download=True)
+    elif dsname == "CIFAR-20":
+        dataset = CIFAR20(iid_path, train=False, transform=transform, download=True)
+    elif dsname == "CIFAR-100":
+        dataset = CIFAR100(iid_path, train=False, transform=transform, download=True)
+    elif dsname == 'Tiny-ImageNet':
+        dataset = TinyImageNet(iid_path, split='test', transform=transform)
+    else:
+        raise ValueError('unknown dataset')
+    
+    # test on corrupted data
+    if corr_sev > 0:
+        if dsname in ['CIFAR-10', 'CIFAR-100']:
+            path_images = os.path.join(corr_path, corr_type + '.npy')
+            path_labels = os.path.join(corr_path, 'labels.npy')
+            dataset.data = np.load(path_images)[(corr_sev - 1) * 10000:corr_sev * 10000]
+            dataset.targets = list(np.load(path_labels)[(corr_sev - 1) * 10000:corr_sev * 10000])
+            dataset.targets = [int(item) for item in dataset.targets]
+        
+        elif dsname == 'CIFAR-20':
+            path_images = os.path.join(corr_path, corr_type + '.npy')
+            path_labels = os.path.join(corr_path, 'labels.npy')
+            dataset.data = np.load(path_images)[(corr_sev - 1) * 10000:corr_sev * 10000]
+            dataset.targets = list(get_coarse_labels(np.load(path_labels))[(corr_sev - 1) * 10000:corr_sev * 10000])
+            dataset.targets = [int(item) for item in dataset.targets]
+
+        elif dsname == 'Tiny-ImageNet':
+            dataset = TinyImageNetCorrupted(corr_path, corr_type, corr_sev, transform=transform)
+
+    # randomly subsample test set to see sample complexity
+    if n_test_sample < 10000:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        indices = torch.randperm(10000)[:n_test_sample]
+        dataset = torch.utils.data.Subset(dataset, indices)
+        print('number of test data: ', len(dataset))
+
+    return dataset
+        
+    
---
+++ run_estimation.py
@@ -0,0 +1,136 @@
+import argparse
+from projnorm import *
+from load_data import load_train_dataset, load_test_dataset
+from model import ResNet18, ResNet50, VGG11
+from misc.temperature_scaling import calibrate
+from collections import Counter
+from utils import gather_outputs
+from misc.torch_interp import interpolate
+from torch_datasets.configs import get_expected_label_distribution
+import numpy as np
+import json
+import torch
+import math
+import os
+from tqdm import tqdm
+import ot
+import ot.dr
+import torch.nn as nn
+
+
+def main():
+    # generic configs
+    parser = argparse.ArgumentParser(description='Estimate target domain performance.')
+    parser.add_argument('--arch', default='resnet18', type=str)
+    parser.add_argument('--metric', default='EMD', type=str)
+    parser.add_argument('--dataset', default='cifar-10', type=str)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--n_val_samples', default=10000, type=int)
+    parser.add_argument('--n_test_samples', default=1000, type=int)
+    parser.add_argument('--dataset_seed', default=1, type=int)
+    parser.add_argument('--model_seed', default=1, type=int)
+    parser.add_argument('--ckpt_epoch', default=20, type=int)
+
+    # synthetic shifts configs
+    parser.add_argument('--data_path', default='./data/CIFAR-10', type=str)
+    parser.add_argument('--corruption_path', default='./data/CIFAR-10-C/', type=str)
+    parser.add_argument('--corruption', default='brightness', type=str)
+    parser.add_argument('--severity', default=1, type=int)
+    
+    args = parser.parse_args()
+
+    print(vars(args))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_seed = args.model_seed
+    n_test_sample = args.n_test_samples
+    dsname = args.dataset
+    corruption = args.corruption
+    severity = args.severity
+
+    _, val_set = load_train_dataset(dsname=dsname,
+                                    iid_path=args.data_path,
+                                    n_val_samples=args.n_val_samples,
+                                    seed=args.dataset_seed)
+
+    val_iid_loader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=False)
+
+    valset_ood = load_test_dataset(dsname=dsname,
+                                   iid_path=args.data_path,
+                                   corr_path=args.corruption_path,
+                                   corr_type=args.corruption,
+                                   corr_sev=args.severity,
+                                   n_test_sample=n_test_sample)
+
+    val_ood_loader = torch.utils.data.DataLoader(valset_ood, batch_size=args.batch_size, shuffle=True)
+
+    cache_dir = f"./cache/{dsname}/{args.arch}_{model_seed}"
+    os.makedirs(cache_dir, exist_ok=True)
+    cache_id_dir = f"{cache_dir}/id_{model_seed}_d{args.dataset_seed}.pkl"
+    cache_od_dir = f"{cache_dir}/od_{model_seed}_{args.corruption}-{args.severity}_n{n_test_sample}.pkl"
+
+    save_dir_path = f"./checkpoints/{dsname}/{args.arch}"
+
+    base_model = torch.load(f"{save_dir_path}/base_model_{args.model_seed}.pt", map_location=device)
+    model = base_model.eval()
+
+    temp_dir = f"{save_dir_path}/base_model_{args.model_seed}_temp.json"
+    model = calibrate(model, val_iid_loader, temp_dir)
+
+    iid_acts, iid_preds, iid_tars = gather_outputs(model, val_iid_loader, device, cache_id_dir)
+    ood_acts, ood_preds, ood_tars = gather_outputs(model, val_ood_loader, device, cache_od_dir)
+
+    iid_acc = ( (iid_preds == iid_tars).sum() / len(iid_tars) ).item()
+    ood_acc = ( (ood_preds == ood_tars).sum() / len(ood_tars) ).item()
+
+    conf = torch.nn.functional.softmax(iid_acts, dim=1).amax(1).mean().item()
+    conf_gap =  conf - iid_acc
+
+    print('validation acc:', iid_acc)
+    print('validation confidence:', conf)
+    print('confidence gap:', conf - iid_acc)
+    print('out-distribution acc:', ood_acc)
+
+    metric = args.metric
+
+    if metric == 'EMD':
+        act = nn.Softmax(dim=1)
+
+        iid_acts, ood_acts = nn.functional.one_hot(iid_tars), act(ood_acts)
+        
+        M = torch.from_numpy(ot.dist(iid_acts.cpu().numpy(), ood_acts.cpu().numpy(), metric='minkowski', p=1)).to(device)
+        
+        weights = torch.as_tensor([]).to(device)
+
+        dist = ot.emd2(weights, weights, M, numItermax=10**8) / 2 + conf_gap
+
+    print(f'{metric} value:', dist.item())
+
+    result_dir = f"results/{dsname}/{args.arch}_{model_seed}/{args.metric}_{n_test_sample}/{corruption}.json"
+
+    print(result_dir, os.path.dirname(result_dir), os.path.basename(result_dir))
+    os.makedirs(os.path.dirname(result_dir), exist_ok=True)
+
+    if not os.path.exists(result_dir):
+        with open(result_dir, 'w') as f:
+            json.dump([], f)
+
+    with open(result_dir, 'r') as f:
+        data = json.load(f)
+    
+    data.append({
+        'corruption': corruption,
+        'corruption level': severity,
+        'metric': float(dist),
+        'ref': metric,
+        'acc': float(ood_acc),
+        'error': 1 - ood_acc,
+    })
+
+    with open(result_dir, 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    main()
+
