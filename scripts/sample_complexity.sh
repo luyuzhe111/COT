4fc58a257dffc6a07bbe236afc75f904a26f1cea
luyuzhe111 <yuzhe.lu@vanderbilt.edu>
2023-01-31 14:57:30 -0800

    gather sensitivity results

--- scripts/sample_complexity.sh
+++ scripts/sample_complexity.sh
@@ -12,25 +12,25 @@ conda activate ood
 cd /usr/workspace/lu35/Documents/fot
 
 metric="EMD"
-data_path="./data/CIFAR-10"
-dataset="CIFAR-10"
-corruption_path="./data/CIFAR-10-C"
+data_path="./data/Tiny-ImageNet"
+dataset="Tiny-ImageNet"
+corruption_path="./data/Tiny-ImageNet-C"
 n_val_samples=10000
 batch_size=128
-arch=resnet18
+arch=resnet50
 model_seed=1
 
 if [[ ${dataset} == "CIFAR-10" ]] || [[ ${dataset} == "CIFAR-100" ]]
 then
     corruptions="brightness defocus_blur elastic_transform fog frost gaussian_blur gaussian_noise glass_blur impulse_noise jpeg_compression motion_blur pixelate saturate shot_noise snow spatter speckle_noise zoom_blur contrast"
-elif [[ ${dataset} == "tiny-imagenet" ]]
+elif [[ ${dataset} == "Tiny-ImageNet" ]]
 then
     corruptions="brightness defocus_blur elastic_transform fog frost gaussian_noise glass_blur impulse_noise jpeg_compression motion_blur pixelate shot_noise snow zoom_blur contrast" 
 fi
 
 echo ${corruptions}
 
-for n_test_samples in 5000 # 2000 1000 500 200 100
+for n_test_samples in 5000 2000 1000 500 200 100
     do
         python run_estimation.py --dataset ${dataset} --corruption clean --severity 0 --model_seed ${model_seed} --n_test_samples ${n_test_samples} --batch_size ${batch_size} --arch ${arch} --metric ${metric} --data_path ${data_path} --corruption_path ${corruption_path}
 
