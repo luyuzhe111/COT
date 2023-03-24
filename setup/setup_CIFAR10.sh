7f2e181fd828485736867bde5bac0acf27db26aa
luyuzhe111 <yuzhe.lu@vanderbilt.edu>
2023-03-23 21:52:11 -0700

    add cifar setup

---
+++ setup/setup_CIFAR10.sh
@@ -0,0 +1,12 @@
+mkdir ./data/CIFAR-10/
+cd ./data/CIFAR-10/
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xvf cifar-10-python.tar.gz
+
+cd ..
+mkdir ./CIFAR-10-C/
+cd ./CIFAR-10-C/
+wget https://zenodo.org/record/2535967/files/CIFAR-10-C.tar
+
+tar -xvf CIFAR-10-C.tar
+
